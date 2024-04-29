369adfa7f8a47236b5f89b4d6b52f6344b2f2917
PastelQ <a0941@naver.com>
2024-04-29 19:33:35 +0900

    20240429 머지 올리기 전

--- ccm/src/main/java/mypage/handler/EditMyProfileUpdateHandler.java
+++ ccm/src/main/java/mypage/handler/EditMyProfileUpdateHandler.java
@@ -25,8 +25,8 @@ public class EditMyProfileUpdateHandler extends HttpServlet implements CommandHa
 	public static final String FORM_INDEX = "/main.do";
 	public static final String FORM_MYPAGE = "/mypage.do";
 	//본인의 파일업로드경로
-	//프로필폴더 우클릭 > 프로퍼리 > 로케이션복사
-	public static final String FILE_ROOT_PATH = "C:/Users/user/git/CaffeineCheckMate/ccm/src/main/webapp/resources/profile";
+	//프로필폴더 우클릭 > 프로퍼리 > 로케이션복사 C:\Users\USER\git\CaffeineCheckMate\ccm\src\main\webapp\resources\profile
+	public static final String FILE_ROOT_PATH = "C:/Users/USER/git/CaffeineCheckMate/ccm/src/main/webapp/resources/profile";
 	// 애플리케이션 루트 경로 얻기
 	//public static final String APP_ROOT = req.getServletContext().getRealPath("/");
 	
